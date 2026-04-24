a12fe827284cb32414b67cf17ce6778687fcf831
Waleed Khamees <walid.osama.khamees@gmail.com>
2026-04-24 07:46:59 +0300

    car: adds basic function

---
+++ solution.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+class Car {
+private: 
+    // engine 
+public: 
+    void start() {
+    }
+    void stop() {
+    }
+    void accerlate() {
+    }
+    void brake() {
+    }
+};
+
+int main() {
+
+    return 0;
+}
