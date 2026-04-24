01480073a76db94c93c9f93affe1fa7ea37d57a5
Waleed Khamees <walid.osama.khamees@gmail.com>
2026-04-24 07:57:22 +0300

    adds basic blueprints for the engine and the its specialized types

--- solution.cpp
+++ solution.cpp
@@ -1,5 +1,59 @@
 #include <iostream>
 
+class Engine {
+public:
+    virtual void increase() = 0;
+    virtual void decrease() = 0;
+};
+
+class GasEngine : Engine {
+    int speed;
+public: 
+    GasEngine() {
+        speed = 0; 
+    }
+    void increase() {
+    }
+    void decrease() {
+    }
+    ~GasEngine() {
+    }
+};
+
+class ElectricEngine : Engine {
+    int speed;
+public: 
+    ElectricEngine() {
+        speed = 0; 
+    }
+    void increase() {
+    }
+    void decrease() {
+    }
+    ~ElectricEngine() {
+    }
+};
+
+class HybirdEngine : Engine {
+    int speed = 0; 
+    GasEngine gasEngine; 
+    ElectricEngine elecEngine; 
+public: 
+    HybirdEngine() {
+        gasEngine = new GasEngine()
+        elecEngine = new ElectricEngine()
+    }
+    void increase() {
+    }
+    void decrease() {
+    }
+
+    ~HybirdEngine() {
+        delete gasEngine
+        delete elecEngine
+    }
+};
+
 class Car {
 private: 
     // engine 
