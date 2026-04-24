0bedbcb416b58bd87b33d74dd6a2d9100f44b0b8
Waleed Khamees <walid.osama.khamees@gmail.com>
2026-04-24 11:24:08 +0300

    adds acc, brake, stop functionality for all engines.

--- solution.cpp
+++ solution.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
+#include <math.h>
 
-enum EngineType {
-    GAS, 
-    ELECTRIC, 
-    HYBRID 
+#define MAX_SPEED 200
+#define SPEED_CHANGE_AMOUNT 20
+// When the hybrid motor switches from electric to gas engine
+#define HYBRID_SWITCH 50
+
+enum class EngineType {
+    Gas, 
+    Electric, 
+    Hybrid 
 };
 
 class Engine {
@@ -24,14 +30,10 @@ public:
         speed = 0; 
     }
     void increase() {
-        if (speed < 200) {
-            speed += 1;
-        }
+        speed += 1;
     }
     void decrease() {
-        if (speed > 0) {
-            speed -= 1;
-        }
+        speed -= 1;
     }
     ~GasEngine() {
         std::cout << "LOG: Engine destructor called\n";
@@ -44,14 +46,10 @@ public:
         speed = 0; 
     }
     void increase() {
-        if (speed < 200) {
-            speed += 1;
-        }
+        speed += 1;
     }
     void decrease() {
-        if (speed > 0) {
-            speed -= 1;
-        }
+        speed -= 1;
     }
     ~ElectricEngine() {
         std::cout << "LOG: Engine destructor called\n";
@@ -63,18 +61,71 @@ class HybirdEngine : public Engine {
     ElectricEngine* elecEngine; 
 public: 
     HybirdEngine() {
-        speed = 0; 
         gasEngine = new GasEngine();
         elecEngine = new ElectricEngine();
     }
+
     void increase() {
+        int speed = getSpeed();
+        bool isElectric = gasEngine->getSpeed() == 0;
+
+        switch(isElectric) {
+            case true: {
+                if (speed < HYBRID_SWITCH) {
+                    elecEngine->increase();
+                }
+                else {
+                    std::cout << "LOG: switching to gas\n";
+                    while (elecEngine->getSpeed()){
+                        elecEngine->decrease();
+                    }
+                    for (int i = 0; i < speed+1; i++) {
+                        gasEngine->increase();
+                    }
+                }
+                break;
+            }
+            case false:
+                gasEngine->increase();
+                break;
+        }
+
     }
+    
     void decrease() {
+        int speed = getSpeed();
+        bool isGas = elecEngine->getSpeed() == 0;
+        if (speed == 0) {
+            return;
+        }
+
+        switch(isGas) {
+            case true: {
+                if (speed >= HYBRID_SWITCH) {
+                    gasEngine->decrease();
+                }
+                else {
+                    std::cout << "LOG: switching to electric\n";
+                    while (gasEngine->getSpeed()){
+                        gasEngine->decrease();
+                    }
+
+                    for (int i = 0; i < speed-1; i++) {
+                        elecEngine->increase();
+                    }
+                }
+                break;
+            }
+            case false:
+                elecEngine->decrease();
+                break;
+        }
     }
+
     int getSpeed() override {
-        // something 
-        return 0;
+        return std::max(elecEngine->getSpeed(), gasEngine->getSpeed());
     }
+
     ~HybirdEngine() {
         std::cout << "LOG: Engine destructor called\n";
         delete gasEngine;
@@ -92,20 +143,27 @@ public:
     void start() {
         std::cout << "LOG: car started\n";
     }
+
     void stop() {
         while (engine->getSpeed()) {
             engine->decrease();
         }
         std::cout << "LOG: Car's speed: " << engine->getSpeed() << "\n";
     }
+
     void accerlate() {
-        for (int i = 0; i < 20; i++) {
+        if (engine->getSpeed() == MAX_SPEED) {
+            std::cout << "LOG: max engine speed reached, speed: " << engine->getSpeed() << "\n";
+            return;
+        }
+
+        for (int i = 0; i < SPEED_CHANGE_AMOUNT && engine->getSpeed() < MAX_SPEED; i++) {
             engine->increase();
         }
         std::cout << "LOG: Car's speed: " << engine->getSpeed() << "\n";
     }
     void brake() {
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < SPEED_CHANGE_AMOUNT && engine->getSpeed() >= 0; i++) {
             engine->decrease();
         }
         std::cout << "LOG: Car's speed: " << engine->getSpeed() << "\n";
@@ -118,15 +176,15 @@ public:
 
 Car createCar(EngineType type) {
     switch (type) {
-        case GAS: {
+        case EngineType::Gas: {
             auto gasEngine = new GasEngine();
             return Car(gasEngine);
         }
-        case ELECTRIC: {
+        case EngineType::Electric: {
             auto elecEngine = new ElectricEngine();
             return Car(elecEngine);
         }
-        case HYBRID: {
+        case EngineType::Hybrid: {
             auto hybird = new HybirdEngine();
             return Car(hybird);
         }
@@ -137,15 +195,17 @@ Car createCar(EngineType type) {
 }
 
 int main() {
-    auto car = createCar(GAS);
+    auto car = createCar(EngineType::Hybrid);
     
     car.start();
-    car.accerlate();
-    car.accerlate();
+    for (int i = 0; i < 11; i++) {
+        car.accerlate();
+    }
 
-    car.brake();
+    for (int i = 0; i < 3; i++) {
+        car.brake();
+    }
 
     car.stop();
-
     return 0;
 }
